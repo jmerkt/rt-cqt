478f26931d7cec5dc4be75a6a90fa41c72378972
jmerkt <jmerkt190@gmail.com>
2024-10-05 14:11:46 +0200

    Refactor examples a bit.

--- examples/cqt.cpp
+++ examples/cqt.cpp
@@ -9,25 +9,25 @@ int main(int argc, char *argv[])
     const int block_size = 1024;
     const double sample_rate = 48000.;
 
-    std::vector<double> audioInputBlock(block_size, 0.);
-    std::vector<double> audioOutputBlock(block_size, 0.);
+    std::vector<double> audio_input_block(block_size, 0.);
+    std::vector<double> audio_output_block(block_size, 0.);
 
     Cqt::ConstantQTransform<bins_per_octave, octave_number> cqt;
     cqt.init(hop_size); // Separate hop-sizes for each octave can be initialized using the .init(std::vector<int> octaveHopSizes) overload
     cqt.initFs(sample_rate, block_size);
 
-    cqt.inputBlock(audioInputBlock.data(), block_size);
-    auto schedule = cqt.getCqtSchedule();
+    cqt.inputBlock(audio_input_block.data(), block_size);
+    const auto &schedule = cqt.getCqtSchedule();
     for (const auto &s : schedule)
     {
         cqt.cqt(s);
-        auto cqtDomainBuffer = cqt.getOctaveCqtBuffer(s.octave()); // The data could now be manipulated in cqt domain
+        auto cqt_domain_buffer = cqt.getOctaveCqtBuffer(s.octave()); // The data could now be manipulated in cqt domain
         cqt.icqt(s);
     }
-    auto cqtAudioBlock = cqt.outputBlock(audioInputBlock.size());
-    for (int i = 0; i < audioInputBlock.size(); i++)
+    auto cqt_audio_block = cqt.outputBlock(audio_input_block.size());
+    for (size_t i = 0U; i < audio_input_block.size(); ++i)
     {
-        audioOutputBlock[i] = cqtAudioBlock[i];
+        audio_output_block[i] = cqt_audio_block[i];
     }
 
     return 0;
--- examples/scqt.cpp
+++ examples/scqt.cpp
@@ -6,49 +6,50 @@ int main(int argc, char *argv[])
     constexpr int bins_per_octave = 12;
     constexpr bool use_windowing = false;
 
-    const int blockSize = 1024;
-    const double samplerate = 48000.;
+    const int block_size = 1024;
+    const double sample_rate = 48000.;
 
-    std::vector<double> audioInputBlock(blockSize, 0.);
-    std::vector<double> audioOutputBlock(blockSize, 0.);
-    std::vector<std::complex<double>> cqtDomainBuffers[octave_number][bins_per_octave];
+    std::vector<double> audio_input_block(block_size, 0.);
+    std::vector<double> audio_output_block(block_size, 0.);
+    std::vector<std::complex<double>> cqt_domain_buffers[octave_number][bins_per_octave];
 
     Cqt::SlidingCqt<bins_per_octave, octave_number, use_windowing> cqt;
-    cqt.init(samplerate, blockSize);
+    cqt.init(sample_rate, block_size);
 
-    for (unsigned i_octave = 0u; i_octave < octave_number; i_octave++)
+    for (int octave = 0; octave < octave_number; ++octave)
     {
         // The sample rates and block sizes of each downsampled octave can be accessed
-        const double octaveRate = cqt.getOctaveSampleRate(i_octave);
-        const int octaveSize = cqt.getOctaveBlockSize(i_octave);
-        for (unsigned i_tone = 0u; i_tone < bins_per_octave; i_tone++)
+        const double octave_rate = cqt.getOctaveSampleRate(octave);
+        const int octave_size = cqt.getOctaveBlockSize(octave);
+        for (int tone = 0; tone < bins_per_octave; ++tone)
         {
-            cqtDomainBuffers[i_octave][i_tone].resize(octaveSize, {0., 0.});
+            cqt_domain_buffers[octave][tone].resize(octave_size, {0., 0.});
         }
     }
 
-    cqt.inputBlock(audioInputBlock.data(), blockSize);
-    for (unsigned i_octave = 0u; i_octave < octave_number; i_octave++)
+    cqt.inputBlock(audio_input_block.data(), block_size);
+    for (int octave = 0; octave < octave_number; ++octave)
     {
-        // All complex values for each octave and bin are stored in circular buffers and can be accessed by getting a pointer to that buffer.
         // Because the data for each octave is downsampled, the number of samples per octave and block varies.
-        const size_t nSamplesOctave = cqt.getSamplesToProcess(i_octave);
-        audio_utils::CircularBuffer<std::complex<double>> *octaveCqtBuffer = cqt.getOctaveCqtBuffer(i_octave);
-        for (unsigned i_tone = 0u; i_tone < bins_per_octave; i_tone++)
+        const size_t number_octave_samples = cqt.getSamplesToProcess(octave);
+        for (int tone = 0; tone < bins_per_octave; ++tone)
         {
-            octaveCqtBuffer[i_tone].pullBlock(cqtDomainBuffers[i_octave][i_tone].data(), nSamplesOctave);
-            for (size_t i_sample = 0u; i_sample < nSamplesOctave; i_sample++)
+            // Pull the cqt domain data for this octave and tone
+            cqt.pullBinCqtData(octave, tone, cqt_domain_buffers[octave][tone].data());
+            for (size_t sample = 0U; sample < number_octave_samples; ++sample)
             {
-                // Here we can manipulate the complex values for each sample, bin and octave
-                cqtDomainBuffers[i_octave][i_tone][i_sample] *= 2.0;
+                // Here, we can manipulate the complex values for each sample, bin and octave
+                cqt_domain_buffers[octave][tone][sample] *= 2.0;
             }
-            octaveCqtBuffer[i_tone].pushBlock(cqtDomainBuffers[i_octave][i_tone].data(), nSamplesOctave);
+            // Push back the manipulated cqt domain data for this octave and tone
+            // The number of pulled and pushed samples must match number_octave_samples
+            cqt.pushBinCqtData(octave, tone, cqt_domain_buffers[octave][tone].data());
         }
     }
-    auto cqtAudioBlock = cqt.outputBlock(audioInputBlock.size());
-    for (int i_sample = 0; i_sample < audioInputBlock.size(); i_sample++)
+    auto cqt_audio_block = cqt.outputBlock(audio_input_block.size());
+    for (size_t sample = 0U; sample < audio_input_block.size(); ++sample)
     {
-        audioOutputBlock[i_sample] = cqtAudioBlock[i_sample];
+        audio_output_block[sample] = cqt_audio_block[sample];
     }
 
     return 0;
